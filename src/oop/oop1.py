e0eb8e9683fc718143aaebb718c94e4f95d9e0e6
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-04-10 08:09:11 -0700

    finished opp1 and tests passing

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,37 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# MARK: -- Base class
+class Vehicle:
+		pass
+
+# MARK: -- Subclass of Vehicle
+class FlightVehicle(Vehicle):
+	def __init__(self):
+		pass
+
+# MARK: -- Subclass of Vehicle
+class Starship(FlightVehicle):
+	def __init__(self):
+		pass
+
+# MARK: -- Subclass of FlightVehicle
+class Airplane(FlightVehicle):
+	def __init__(self, ):
+		pass
+
+# MARK: -- Subclass of Vehicle
+class GroundVehicle(Vehicle):
+	def __init__(self):
+		pass
+
+# MARK: -- Subclass of GroundVehicle
+class Car(GroundVehicle):
+	def __init__(self):
+		pass
+
+# MARK: -- Subclass of GroundVehicle
+class Motorcycle(GroundVehicle):
+	def __init__(self):
+		pass
