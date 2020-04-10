2f018805dccc262eb70cb7ea81d17ee41aa0ac33
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-04-10 09:59:55 -0700

    added class and cvs reader. skipped header

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,18 @@
+# MARK: -- Modules
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
+  def __repr__(self):
+    return f"({self.name}, {self.lat}, {self.lon})"
+
+  def __str__(self):
+    return f"({self.name}, {self.lat}, {self.lon})"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -20,8 +32,13 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+  with open('cities.csv') as city_file:
+    reader = csv.reader(city_file)
+    next(reader)
+    for row in reader:
+      (name, lat, lon) = (row[0], row[3], row[4])
+      cities.append(City(name, lat, lon))
+  return cities
 
 cityreader(cities)
 
